206512fb665f6712217717e457fc930390377aa6
Asif <machoasif@zoho.com>
2019-10-01 21:03:25 +0530

    initial commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Muhammed Asif M M on 01/10/19.
+//  Copyright © 2019 Muhammed Asif M M. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
