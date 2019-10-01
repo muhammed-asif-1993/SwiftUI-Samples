92b6801577fe4868ea4f2997648fc2a847df0d14
Asif <machoasif@zoho.com>
2019-10-01 23:30:33 +0530

    started learning SwiftUI.

--- SwiftUI Demo/ContentView.swift
+++ SwiftUI Demo/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI")
+        Text("Muhammed Asif").font(.title).foregroundColor(.blue)
     }
 }
 
