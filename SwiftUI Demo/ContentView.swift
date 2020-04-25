04bc3808152c4b1fe108b310865c30155f476af4
Asif <muhammed.asif@fingent.com>
2020-04-25 13:10:47 +0530

    ~ new changes in 2020

--- SwiftUI Demo/ContentView.swift
+++ SwiftUI Demo/ContentView.swift
@@ -10,22 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            TopView()
-                .edgesIgnoringSafeArea(.top)
-                .frame(height: 200)
-            ProfileImageView()
-                .offset(y: -100)
-                .padding(.bottom, -100)
-            VStack(alignment: .center) {
-                Text("Muhammed Asif").font(.headline).foregroundColor(.blue)
-                HStack(alignment: .top, spacing: 8) {
-                    Text("iOS Developer").font(.subheadline).foregroundColor(.gray)
-                    Text("Fingent Global Solutions").font(.subheadline)
-                }
-            }
-            Spacer()
-        }
+        Text("Hello SwiftUI!")
     }
 }
 
